bd5e055fd76e067b8d4a7e3f1d4ff6af9dbe2285
Rahmat Sholeh <38641053+dvg32@users.noreply.github.com>
2024-01-29 11:21:58 +0800

    mail tables migration

---
+++ database/migrations/2024_01_29_020119_add_col_in_incoming_mails.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('incoming_mails', function (Blueprint $table) {
+            //
+            $table->string('mail_code', 6);
+            $table->date('input_date');
+            $table->text('mail_subject');
+            $table->date('mail_date');
+            $table->text('document_location');
+            $table->boolean('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('incoming_mails', function (Blueprint $table) {
+            //
+            $table->dropColumn('mail_code');
+            $table->dropColumn('input_date');
+            $table->dropColumn('mail_subject');
+            $table->dropColumn('mail_date');
+            $table->dropColumn('document_location');
+            $table->dropColumn('status');
+        });
+    }
+};
