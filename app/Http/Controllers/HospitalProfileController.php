d8ac78cb8d03f46b1802722ae8e216d280b070d2
Ragil M Rivandi <50643466+immedy@users.noreply.github.com>
2024-01-31 10:44:08 +0800

    updatefrontend

--- app/Http/Controllers/HospitalProfileController.php
+++ app/Http/Controllers/HospitalProfileController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\mail;
-use Illuminate\Http\Request;
 use App\Models\hospital_profile;
 use App\Http\Requests\Storehospital_profileRequest;
 use App\Http\Requests\Updatehospital_profileRequest;
@@ -13,32 +12,15 @@ class HospitalProfileController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
-{
-    $currentYear = date('Y');
-
-    // Mendapatkan tahun yang dipilih dari permintaan (request)
-    $selectedYear = $request->input('year', $currentYear);
-
-    // Mendapatkan data surat berdasarkan tahun yang dipilih
-    $mailData = Mail::whereYear('mail_date', $selectedYear)->paginate(100);
-
-    // Menghitung total surat
-    $mailCount = $mailData->total();
-
-    // Menghitung surat masuk dan surat keluar
-    $mailInCount = $mailData->where('mail_type', 1)->count();
-    $mailOutCount = $mailData->where('mail_type', 2)->count();
-
-    return view('HospitalProfile.HospitalProfileIndex', [
-        'Mail' => $mailData,
-        'mailCount' => $mailCount,
-        'MailIn' => $mailInCount,
-        'MailOut' => $mailOutCount,
-        'selectedYear' => $selectedYear, // Untuk mengirimkan tahun yang dipilih ke tampilan
-        'currentYear' => $currentYear, // Untuk mengirimkan tahun saat ini ke tampilan
-    ]);
-}
+    public function index()
+    {
+        return view('HospitalProfile.HospitalProfileIndex',[
+            'Mail' => mail::paginate(100),
+            'mailCount' =>Mail::count(),
+            'MailIn' => Mail::where('mail_type', 1)->count(),
+            'MailOut' => Mail::where('mail_type', 2)->count(),
+        ]);
+    }
 
     /**
      * Show the form for creating a new resource.
