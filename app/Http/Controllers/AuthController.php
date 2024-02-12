08b6f37d70a825b5dd103f243e0de891bd72a717
Rahmat Sholeh <38641053+dvg32@users.noreply.github.com>
2024-02-12 15:06:48 +0800

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class AuthController extends Controller
 {
@@ -19,6 +21,34 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
+    }
+
+    public function Authentication(Request $request)
+    {
+        $validateLogin = $request->validate([
+            'name' => ['required'],
+            'password' => ['required'],
+        ]);
+        if (Auth::attempt($validateLogin)) {
+            # code...
+            $validateLogin = Auth()->user();
+            if ($validateLogin->status == '1' && $validateLogin->role == '1') {
+                # code...
+            } elseif ($validateLogin->status == '1' && $validateLogin->role == '2') {
+                # code...
+            } elseif ($validateLogin->status == '1' && $validateLogin->role == '3') {
+                # code...
+                $request->session()->regenerate();
+                Alert::Toast('Selamat Datang', 'success');
+                return redirect()->route('dashboard');
+            } else {
+                # code...
+                auth()->logout();
+            }
+
+        }
+        Alert::error('Username atau Password Salah');
+        return back();
     }
 
 }
