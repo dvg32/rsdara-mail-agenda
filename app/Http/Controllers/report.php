f2553461f44f89f691e8559fdd83a4f051a56b3b
Rahmat Sholeh <38641053+dvg32@users.noreply.github.com>
2024-02-13 12:46:30 +0800

    Update report.php

--- app/Http/Controllers/report.php
+++ app/Http/Controllers/report.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\mail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class report extends Controller
 {
     public function index()
     {
         return view('Report.report');
+    }
+
+    public function dataGraph()
+    {
+        // return view('HospitalProfile.data-graph');
+        $year = 2023;
+        $monthlyData = mail::select(DB::raw('MONTH(mail_date) as month'), DB::raw('COUNT(*) as count'), 'mail_type')
+            ->whereYear('mail_date', $year)
+            ->groupBy(DB::raw('MONTH(mail_date)'), 'mail_type')
+            ->get();
+        return view('HospitalProfile.data-graph', compact(['monthlyData', 'year']));
     }
 }
