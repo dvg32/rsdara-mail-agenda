28aab14b59d10da20eefbe08d2e59d597f8d4635
Rahmat Sholeh <38641053+dvg32@users.noreply.github.com>
2024-02-15 12:23:50 +0800

    Update report.php

--- app/Http/Controllers/report.php
+++ app/Http/Controllers/report.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\mail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class report extends Controller
@@ -16,7 +17,7 @@ class report extends Controller
     public function dataGraph()
     {
         // return view('HospitalProfile.data-graph');
-        $year = 2023;
+        $year = Carbon::now()->format('Y');
         $monthlyData = mail::select(DB::raw('MONTH(mail_date) as month'), DB::raw('COUNT(*) as count'), 'mail_type')
             ->whereYear('mail_date', $year)
             ->groupBy(DB::raw('MONTH(mail_date)'), 'mail_type')
