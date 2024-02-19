5b4852bb39ae623117a7285f4dac88add8393c6a
Rahmat Sholeh <38641053+dvg32@users.noreply.github.com>
2024-02-19 13:11:21 +0800

    Update MailGuide.blade.php

--- resources/views/Reference/MailGuide.blade.php
+++ resources/views/Reference/MailGuide.blade.php
@@ -1,3 +1,29 @@
 @extends('Layout.Dashboard')
 @section('DASHBOARD')
+<div class="app-main flex-column flex-row-fluid">
+    <!--begin::Container-->
+    <div id="kt_content_container" class="container-fluid">
+
+        <div class="card card-xl-stretch mb-5 mb-xl-8 mt-6">
+            <!--begin::Header-->
+            <div class="card-header border-0 pt-5">
+                <h3 class="card-title align-items-start flex-column">
+                    <span class="card-label fw-bolder fs-3 mb-1">Panduan Surat Menyurat</span>
+
+                </h3>
+
+            </div>
+            <!--end::Header-->
+            <!--begin::Body-->
+            <div class="card-body py-3">
+                <!--begin::Table container-->
+                <iframe src="{{ route('mail-guide-data') }}" frameborder="0" width="100%" height="600"></iframe>
+                <!--end::Table container-->
+            </div>
+            <!--begin::Body-->
+
+
+        </div>
+    </div>
+</div>
 @endsection
