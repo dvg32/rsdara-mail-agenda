98ba877efe849395170c511e5c3bd31503be9a54
Rahmat Sholeh <38641053+dvg32@users.noreply.github.com>
2024-02-20 13:39:34 +0800

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'AppUserAuth' => \App\Http\Middleware\AppUserAuth::class,
         'AppAdminAuth' => \App\Http\Middleware\AppAdminAuth::class,
         'AppSupervisorAuth' => \App\Http\Middleware\AppSupervisorAuth::class,
+        'AppSupervisorUserAuth' => \App\Http\Middleware\AppSupervisorUserAuth::class,
     ];
 }
