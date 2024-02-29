bda41aa961df818731807ad4ac27dfadf196d5b4
Rahmat Sholeh <38641053+dvg32@users.noreply.github.com>
2024-02-29 13:39:30 +0800

    Update ReferenceController.php

--- app/Http/Controllers/ReferenceController.php
+++ app/Http/Controllers/ReferenceController.php
@@ -22,9 +22,9 @@ class ReferenceController extends Controller
         return view('Reference.MailGuide');
     }
 
-    public function dataPanduanSurat()
+    public function dataPanduanSuratTataNaskahDinas()
     {
-        $path = public_path('assets/pdf/panduan.pdf');
+        $path = public_path('assets/pdf/PMDN-No-1-Tahun-2023.pdf');
 
         if (!file_exists($path)) {
             # code...
@@ -35,6 +35,35 @@ class ReferenceController extends Controller
 
         return response($content)->header('Content-Type', 'application/pdf');
     }
+
+    public function dataPanduanSuratPermendagri83Tahun2022()
+    {
+        $path = public_path('assets/pdf/permendagri-83-Tahun-2022-tentang-Kode-Klasifikasi-Arsip-kode-surat.pdf');
+
+        if (!file_exists($path)) {
+            # code...
+            abort(404, 'FIle not found');
+        }
+
+        $content = file_get_contents($path);
+
+        return response($content)->header('Content-Type', 'application/pdf');
+    }
+
+    public function dataPanduanSuratPerbupNo14Tahun2018()
+    {
+        $path = public_path('assets/pdf/PERBUP NO 14 TAHUN 2018 TENTANG TATA NASKAH DINAS DI PEMERINTAH KABUPATEN KUTAI KARTANEGARA.pdf');
+
+        if (!file_exists($path)) {
+            # code...
+            abort(404, 'FIle not found');
+        }
+
+        $content = file_get_contents($path);
+
+        return response($content)->header('Content-Type', 'application/pdf');
+    }
+
 
     /**
      * Show the form for creating a new resource.
