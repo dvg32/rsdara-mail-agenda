e27b1f0651196bfd5a43f673c8556ae235f8dcc0
Ragil M Rivandi <50643466+immedy@users.noreply.github.com>
2024-03-05 17:26:52 +0800

    auth

--- routes/web.php
+++ routes/web.php
@@ -20,46 +20,46 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(AdminController::class)->group(function(){
-    route::get('/admin_index', 'index')->name('admin_index');
-    route::get('/user_management', 'user_management')->name('user_management');
-    route::get('/employee_management', 'employee_management')->name('employee_management');
-    route::post('/addEmployee','addEmployee')->name('addEmployee');
-    route::get('/searchEmployeeUsername/{id}','searchEmployeeUsername')->name('searchEmployeeUsername');
-    route::put('/addUsername','addUsername')->name('addUsername');
+    route::get('/admin_index', 'index')->name('admin_index')->middleware('auth');
+    route::get('/user_management', 'user_management')->name('user_management')->middleware('auth');
+    route::get('/employee_management', 'employee_management')->name('employee_management')->middleware('auth');
+    route::post('/addEmployee','addEmployee')->name('addEmployee')->middleware('auth');
+    route::get('/searchEmployeeUsername/{id}','searchEmployeeUsername')->name('searchEmployeeUsername')->middleware('auth');
+    route::put('/addUsername','addUsername')->name('addUsername')->middleware('auth');
 });
 
 Route::controller(HospitalProfileController::class)->group(function(){
-    route::get('/','index');
-    route::get('/supervisorIndex','supervisorIndex')->name('supervisorIndex');
-    route::get('/incomingMail', 'incomingMail')->name('incomingMail');
-    route::get('/outgoingMail', 'outgoingMail')->name('outgoingMail');
-    route::get('/mail-data', 'getMonthlyData')->name('mail-data');
+    route::get('/','index')->middleware('auth');
+    route::get('/supervisorIndex','supervisorIndex')->name('supervisorIndex')->middleware('auth');
+    route::get('/incomingMail', 'incomingMail')->name('incomingMail')->middleware('auth');
+    route::get('/outgoingMail', 'outgoingMail')->name('outgoingMail')->middleware('auth');
+    route::get('/mail-data', 'getMonthlyData')->name('mail-data')->middleware('auth');
 });
 
 Route::controller(report::class)->group(function(){
-    route::get('/data-graph', 'dataGraph')->name('data-graph');
+    route::get('/data-graph', 'dataGraph')->name('data-graph')->middleware('auth');
 });
 
 Route::controller(MailController::class)->group(function(){
-    route::post('/input-mail', 'store')->name('input-mail');
-    route::get('/mail-data', 'getMonthlyData')->name('mail-data');
+    route::post('/input-mail', 'store')->name('input-mail')->middleware('auth');
+    route::get('/mail-data', 'getMonthlyData')->name('mail-data')->middleware('auth');
 });
 
 route::controller(report::class)->group(function(){
-    route::get('/Report','index');
+    route::get('/Report','index')->middleware('auth');
 });
 
 route::controller(ReferenceController::class)->group(function(){
-    route::get('/Reference','index')->name('Reference');
-    route::get('/mail-guide','panduanSurat')->name('mail-guide');
-    // route::get('/mail-guide-data','dataPanduanSurat')->name('mail-guide-data');
-    route::get('/dataPanduanSuratTataNaskahDinas','dataPanduanSuratTataNaskahDinas')->name('dataPanduanSuratTataNaskahDinas');
-    route::get('/dataPanduanSuratPermendagri83Tahun2022','dataPanduanSuratPermendagri83Tahun2022')->name('dataPanduanSuratPermendagri83Tahun2022');
-    route::get('/dataPanduanSuratPerbupNo14Tahun2018','dataPanduanSuratPerbupNo14Tahun2018')->name('dataPanduanSuratPerbupNo14Tahun2018');
+    route::get('/Reference','index')->name('Reference')->middleware('auth');
+    route::get('/mail-guide','panduanSurat')->name('mail-guide')->middleware('auth');
+    // route::get('/mail-guide-data','dataPanduanSurat')->name('mail-guide-data')->middleware('auth');
+    route::get('/dataPanduanSuratTataNaskahDinas','dataPanduanSuratTataNaskahDinas')->name('dataPanduanSuratTataNaskahDinas')->middleware('auth');
+    route::get('/dataPanduanSuratPermendagri83Tahun2022','dataPanduanSuratPermendagri83Tahun2022')->name('dataPanduanSuratPermendagri83Tahun2022')->middleware('auth');
+    route::get('/dataPanduanSuratPerbupNo14Tahun2018','dataPanduanSuratPerbupNo14Tahun2018')->name('dataPanduanSuratPerbupNo14Tahun2018')->middleware('auth');
 });
 
 route::controller(AuthController::class)->group(function(){
-    route::get('/login', 'Login')->name('login');
-    route::post('/authentication', 'authentication')->name('authentication');
-    route::post('/logout', 'Logout')->name('logout');
+    route::get('/login', 'Login')->name('login')->middleware('auth');
+    route::post('/authentication', 'authentication')->name('authentication')->middleware('auth');
+    route::post('/logout', 'Logout')->name('logout')->middleware('auth');
 });
