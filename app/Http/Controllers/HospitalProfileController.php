884ef47db9c95306235365a47c36d67439224e22
Rahmat Sholeh <38641053+dvg32@users.noreply.github.com>
2024-03-21 11:06:42 +0800

    Update HospitalProfileController.php

--- app/Http/Controllers/HospitalProfileController.php
+++ app/Http/Controllers/HospitalProfileController.php
@@ -12,14 +12,25 @@ use App\Models\technical_instruction;
 use RealRashid\SweetAlert\Facades\Alert;
 use App\Http\Requests\Storehospital_profileRequest;
 use App\Http\Requests\Updatehospital_profileRequest;
+use Illuminate\Support\Facades\Mail as FacadesMail;
 
 class HospitalProfileController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+
+        $keyword = $request->input('searchJuknis');
+        if ($keyword) {
+            $juknis = technical_instruction::where(function ($query) use ($keyword) {
+                $query->where('judul_juknis', 'like', "%$keyword%")->orWhere('waktu_dibuat', 'like', "%$keyword%");
+            })->get();
+        } else {
+            $juknis = technical_instruction::latest()->filter()->paginate(25);
+        }
+
         $year = Carbon::now()->format('Y');
         $monthlyData = mail::select(DB::raw('MONTH(mail_date) as month'), DB::raw('COUNT(*) as count'), 'mail_type')
             ->whereYear('mail_date', $year)
@@ -32,9 +43,24 @@ class HospitalProfileController extends Controller
             'MailOut' => Mail::where('mail_type', 2)->whereYear('mail_date', $year)->count(),
             'monthlyData' => $monthlyData,
             'graph_year' => $year,
-            'technical_instruction' => technical_instruction::latest()->filter()->paginate(25)
+            // 'technical_instruction' => technical_instruction::latest()->filter()->paginate(25)
+            'technical_instruction' => $juknis,
         ]);
     }
+
+    // public function viewJuknis(Request $request)
+    // {
+    //     $keyword = $request->input('searchJuknis');
+    //     if ($keyword) {
+    //         $juknis = technical_instruction::where(function ($query) use ($keyword) {
+    //             $query->where('judul_juknis', 'like', "%$keyword%")->orWhere('waktu_dibuat', 'like', "%$keyword%");
+    //         })->get();
+    //     } else {
+    //         $juknis = technical_instruction::latest()->get();
+    //     }
+
+    //     return view('HospitalProfile.technical_instruction', compact('juknis'));
+    // }
 
     public function supervisorIndex()
     {
@@ -57,18 +83,40 @@ class HospitalProfileController extends Controller
      * Show the form for creating a new resource.
      */
 
-    public function incomingMail()
+    public function incomingMail(Request $request)
     {
-        return view('IncomingMail.IncomingMail', [
-            'Mail' => mail::where('mail_type', 1)->orderBy('mail_date', 'desc')->get(),
-        ]);
+        // return view('IncomingMail.IncomingMail', [
+        //     'Mail' => mail::where('mail_type', 1)->orderBy('mail_date', 'desc')->get(),
+        // ]);
+
+        $keyword = $request->input('searchIncomingMail');
+        if ($keyword) {
+            $Mail = mail::where('mail_type', '=', '1')->where(function($query) use ($keyword) {
+                $query->where('mail_from', 'like', "%$keyword%")->orWhere('mail_subject', 'like', "%$keyword%")->orWhere('mail_number', 'like', "%$keyword%");
+            })->get();
+        } else {
+            $Mail = mail::where('mail_type', 1)->orderBy('mail_date', 'desc')->get();
+        }
+
+        return view('IncomingMail.IncomingMail', compact('Mail'));
     }
 
-    public function outgoingMail()
+    public function outgoingMail(Request $request)
     {
-        return view('OutgoingMail.OutgoingMail', [
-            'Mail' => mail::where('mail_type', 2)->orderBy('mail_date', 'desc')->get(),
-        ]);
+        // return view('OutgoingMail.OutgoingMail', [
+        //     'Mail' => mail::where('mail_type', 2)->orderBy('mail_date', 'desc')->get(),
+        // ]);
+
+        $keyword = $request->input('searchOutgoingMail');
+        if ($keyword) {
+            $Mail = mail::where('mail_type', '1')->where(function($query) use ($keyword) {
+                $query->where('mail_from', 'like', "%$keyword%")->orWhere('mail_subject', 'like', "%$keyword%")->orWhere('mail_number', 'like', "%$keyword%");
+            })->get();
+        } else {
+            $Mail = mail::where('mail_type', 1)->orderBy('mail_date', 'desc')->get();
+        }
+
+        return view('OutgoingMail.OutgoingMail', compact('Mail'));
     }
 
     public function addJuknis(Request $requst)
@@ -79,7 +127,7 @@ class HospitalProfileController extends Controller
             'waktu_dibuat' => 'required',
         ]);
         $validateJuknis['employee_id'] = Auth()->user()->employee_id;
-        $validateJuknis['status'] = 1;      
+        $validateJuknis['status'] = 1;
         technical_instruction::create($validateJuknis);
         if ($validateJuknis) {
             Alert::success('Juknis Berhasil Di tambahkan');
@@ -87,6 +135,7 @@ class HospitalProfileController extends Controller
         return back();
     }
 
-    
+
+
 
 }
